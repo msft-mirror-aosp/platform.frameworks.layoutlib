nxp-imx-android/aosp_platform_frameworks_layoutlib

Pass location of ICU data file to layoutlib native
Layoutlib native now needs the path to the ICU data file instead of the
path to its containing directory.

Bug: 179166690
Test: N/A
Change-Id: I80ce0d073df101e159d79ac1ad0ffb3ac3c295f4

bridge/tests/run_tests.sh
@@ -15,7 +15,7 @@ OUT_INTERMEDIATES=${SCRIPT_DIR}"/../../../../out/soong/.intermediates"
 NATIVE_LIBRARIES=${SCRIPT_DIR}"/../../../../out/host/linux-x86/lib64/"
 SDK=${SCRIPT_DIR}"/../../../../out/host/linux-x86/sdk/sdk*/android-sdk*"
 FONT_DIR=${SCRIPT_DIR}"/../../../../out/host/common/obj/PACKAGING/fonts_intermediates"
-ICU_DIR=${SCRIPT_DIR}"/../../../../out/host/linux-x86/com.android.i18n/etc/icu"
+ICU_DATA_PATH=${SCRIPT_DIR}"/../../../../out/host/linux-x86/com.android.i18n/etc/icu/icudt66l.dat"
 TMP_DIR=$(mktemp -d)
 PLATFORM=${TMP_DIR}/"android"
 
@@ -39,7 +39,7 @@ cp -RL ${TMP_DIR}/res ${PLATFORM}/data
 ${STUDIO_JDK}/bin/java -ea \
     -Dnative.lib.path=${NATIVE_LIBRARIES} \
     -Dfont.dir=${FONT_DIR} \
-    -Dicu.dir=${ICU_DIR} \
+    -Dicu.data.path=${ICU_DATA_PATH} \
     -Dplatform.dir=${PLATFORM} \
     -Dtest_res.dir=${SCRIPT_DIR}/res \
     -Dtest_failure.dir=${OUT_DIR}/${FAILURE_DIR} \

bridge/tests/run_tests_mac.sh
@@ -15,7 +15,7 @@ OUT_INTERMEDIATES=${SCRIPT_DIR}"/../../../../out/soong/.intermediates"
 NATIVE_LIBRARIES=${SCRIPT_DIR}"/../../../../out/host/darwin-x86/lib64/"
 SDK=${SCRIPT_DIR}"/../../../../out/host/darwin-x86/sdk/sdk*/android-sdk*"
 FONT_DIR=${SCRIPT_DIR}"/../../../../out/host/common/obj/PACKAGING/fonts_intermediates"
-ICU_DIR=${SCRIPT_DIR}"/../../../../out/host/darwin-x86/com.android.i18n/etc/icu"
+ICU_DATA_PATH=${SCRIPT_DIR}"/../../../../out/host/darwin-x86/com.android.i18n/etc/icu/icudt66l.dat"
 TMP_DIR=$(mktemp -d -t tmp)
 PLATFORM=${TMP_DIR}/"android"
 
@@ -46,7 +46,7 @@ cp -RL ${TMP_DIR}/res ${PLATFORM}/data
 ${STUDIO_JDK}/bin/java -ea \
     -Dnative.lib.path=${NATIVE_LIBRARIES} \
     -Dfont.dir=${FONT_DIR} \
-    -Dicu.dir=${ICU_DIR} \
+    -Dicu.data.path=${ICU_DATA_PATH} \
     -Dplatform.dir=${PLATFORM} \
     -Dtest_res.dir=${SCRIPT_DIR}/res \
     -Dtest_failure.dir=${OUT_DIR}/${FAILURE_DIR} \

bridge/tests/src/com/android/layoutlib/bridge/intensive/RenderTestBase.java
@@ -101,13 +101,13 @@ public class RenderTestBase {
 
     private static final String NATIVE_LIB_PATH_PROPERTY = "native.lib.path";
     private static final String FONT_DIR_PROPERTY = "font.dir";
-    private static final String ICU_DIR_PROPERTY = "icu.dir";
+    private static final String ICU_DATA_PATH_PROPERTY = "icu.data.path";
     private static final String PLATFORM_DIR_PROPERTY = "platform.dir";
     private static final String RESOURCE_DIR_PROPERTY = "test_res.dir";
 
     private static final String NATIVE_LIB_DIR_PATH;
     private static final String FONT_DIR;
-    private static final String ICU_DIR;
+    private static final String ICU_DATA_PATH;
     protected static final String PLATFORM_DIR;
     private static final String TEST_RES_DIR;
     /** Location of the app to test inside {@link #TEST_RES_DIR} */
@@ -136,7 +136,7 @@ public class RenderTestBase {
 
         NATIVE_LIB_DIR_PATH = getNativeLibDirPath();
         FONT_DIR = getFontDir();
-        ICU_DIR = getIcuDir();
+        ICU_DATA_PATH = getIcuDataPath();
 
         TEST_RES_DIR = getTestResDir();
         if (TEST_RES_DIR == null) {
@@ -199,12 +199,12 @@ public class RenderTestBase {
         return fontDir;
     }
 
-    private static String getIcuDir() {
-        String icuDir = System.getProperty(ICU_DIR_PROPERTY);
-        if (icuDir == null) {
-            icuDir = PLATFORM_DIR + "/../../../../../com.android.i18n/etc/icu";
+    private static String getIcuDataPath() {
+        String icuDataPath = System.getProperty(ICU_DATA_PATH_PROPERTY);
+        if (icuDataPath == null) {
+            icuDataPath = PLATFORM_DIR + "/../../../../../com.android.i18n/etc/icu/icudt66l.dat";
         }
-        return icuDir;
+        return icuDataPath;
     }
 
     private static String getPlatformDir() {
@@ -381,7 +381,7 @@ public class RenderTestBase {
         File attrs = new File(res, "values" + File.separator + "attrs.xml");
         sBridge = new Bridge();
         sBridge.init(ConfigGenerator.loadProperties(buildProp), fontLocation, NATIVE_LIB_DIR_PATH,
-                ICU_DIR, ConfigGenerator.getEnumMap(attrs), getLayoutLog());
+                ICU_DATA_PATH, ConfigGenerator.getEnumMap(attrs), getLayoutLog());
         Bridge.getLock().lock();
         try {
             Bridge.setLog(getLayoutLog());

msft-mirror-aosp/platform.frameworks.layoutlib

bridge/tests/run_tests.sh
@@ -15,7 +15,7 @@ OUT_INTERMEDIATES=${SCRIPT_DIR}"/../../../../out/soong/.intermediates"
 NATIVE_LIBRARIES=${SCRIPT_DIR}"/../../../../out/host/linux-x86/lib64/"
 SDK=${SCRIPT_DIR}"/../../../../out/host/linux-x86/sdk/sdk*/android-sdk*"
 FONT_DIR=${SCRIPT_DIR}"/../../../../out/host/common/obj/PACKAGING/fonts_intermediates"
-ICU_DIR=${SCRIPT_DIR}"/../../../../out/host/linux-x86/com.android.i18n/etc/icu"
+ICU_DATA_PATH=${SCRIPT_DIR}"/../../../../out/host/linux-x86/com.android.i18n/etc/icu/icudt68l.dat"
 TMP_DIR=$(mktemp -d)
 PLATFORM=${TMP_DIR}/"android"
 
@@ -39,7 +39,7 @@ cp -RL ${TMP_DIR}/res ${PLATFORM}/data
 ${STUDIO_JDK}/bin/java -ea \
     -Dnative.lib.path=${NATIVE_LIBRARIES} \
     -Dfont.dir=${FONT_DIR} \
-    -Dicu.dir=${ICU_DIR} \
+    -Dicu.data.path=${ICU_DATA_PATH} \
     -Dplatform.dir=${PLATFORM} \
     -Dtest_res.dir=${SCRIPT_DIR}/res \
     -Dtest_failure.dir=${OUT_DIR}/${FAILURE_DIR} \

bridge/tests/run_tests_mac.sh
@@ -15,7 +15,7 @@ OUT_INTERMEDIATES=${SCRIPT_DIR}"/../../../../out/soong/.intermediates"
 NATIVE_LIBRARIES=${SCRIPT_DIR}"/../../../../out/host/darwin-x86/lib64/"
 SDK=${SCRIPT_DIR}"/../../../../out/host/darwin-x86/sdk/sdk*/android-sdk*"
 FONT_DIR=${SCRIPT_DIR}"/../../../../out/host/common/obj/PACKAGING/fonts_intermediates"
-ICU_DIR=${SCRIPT_DIR}"/../../../../out/host/darwin-x86/com.android.i18n/etc/icu"
+ICU_DATA_PATH=${SCRIPT_DIR}"/../../../../out/host/darwin-x86/com.android.i18n/etc/icu/icudt68l.dat"
 TMP_DIR=$(mktemp -d -t tmp)
 PLATFORM=${TMP_DIR}/"android"
 
@@ -46,7 +46,7 @@ cp -RL ${TMP_DIR}/res ${PLATFORM}/data
 ${STUDIO_JDK}/bin/java -ea \
     -Dnative.lib.path=${NATIVE_LIBRARIES} \
     -Dfont.dir=${FONT_DIR} \
-    -Dicu.dir=${ICU_DIR} \
+    -Dicu.data.path=${ICU_DATA_PATH} \
     -Dplatform.dir=${PLATFORM} \
     -Dtest_res.dir=${SCRIPT_DIR}/res \
     -Dtest_failure.dir=${OUT_DIR}/${FAILURE_DIR} \

bridge/tests/src/com/android/layoutlib/bridge/intensive/RenderTestBase.java
@@ -102,13 +102,13 @@ public class RenderTestBase {
 
     private static final String NATIVE_LIB_PATH_PROPERTY = "native.lib.path";
     private static final String FONT_DIR_PROPERTY = "font.dir";
-    private static final String ICU_DIR_PROPERTY = "icu.dir";
+    private static final String ICU_DATA_PATH_PROPERTY = "icu.data.path";
     private static final String PLATFORM_DIR_PROPERTY = "platform.dir";
     private static final String RESOURCE_DIR_PROPERTY = "test_res.dir";
 
     private static final String NATIVE_LIB_DIR_PATH;
     private static final String FONT_DIR;
-    private static final String ICU_DIR;
+    private static final String ICU_DATA_PATH;
     protected static final String PLATFORM_DIR;
     private static final String TEST_RES_DIR;
     /** Location of the app to test inside {@link #TEST_RES_DIR} */
@@ -137,7 +137,7 @@ public class RenderTestBase {
 
         NATIVE_LIB_DIR_PATH = getNativeLibDirPath();
         FONT_DIR = getFontDir();
-        ICU_DIR = getIcuDir();
+        ICU_DATA_PATH = getIcuDataPath();
 
         TEST_RES_DIR = getTestResDir();
         if (TEST_RES_DIR == null) {
@@ -203,12 +203,12 @@ public class RenderTestBase {
         return fontDir;
     }
 
-    private static String getIcuDir() {
-        String icuDir = System.getProperty(ICU_DIR_PROPERTY);
-        if (icuDir == null) {
-            icuDir = PLATFORM_DIR + "/../../../../../com.android.i18n/etc/icu";
+    private static String getIcuDataPath() {
+        String icuDataPath = System.getProperty(ICU_DATA_PATH_PROPERTY);
+        if (icuDataPath == null) {
+            icuDataPath = PLATFORM_DIR + "/../../../../../com.android.i18n/etc/icu/icudt68l.dat";
         }
-        return icuDir;
+        return icuDataPath;
     }
 
     private static String getPlatformDir() {
@@ -385,7 +385,7 @@ public class RenderTestBase {
         File attrs = new File(res, "values" + File.separator + "attrs.xml");
         sBridge = new Bridge();
         sBridge.init(ConfigGenerator.loadProperties(buildProp), fontLocation, NATIVE_LIB_DIR_PATH,
-                ICU_DIR, ConfigGenerator.getEnumMap(attrs), getLayoutLog());
+                ICU_DATA_PATH, ConfigGenerator.getEnumMap(attrs), getLayoutLog());
         Bridge.getLock().lock();
         try {
             Bridge.setLog(getLayoutLog());