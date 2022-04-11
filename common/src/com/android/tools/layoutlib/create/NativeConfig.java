361cea933d0fee8da32716b118ff02478e94b835
Jerome Gaillard <jgaillard@google.com>
2022-04-11 16:54:22 +0100

    Ignore haptic feedback
    Triggering haptic feedback on views currently triggers an exception.
    Simply ignore the call and return false.
    
    Bug: 228794864
    Test: N/A
    Change-Id: I20d7f0945a90a8dfd1835e43b0af3ad27f95ca14

--- common/src/com/android/tools/layoutlib/create/NativeConfig.java
+++ common/src/com/android/tools/layoutlib/create/NativeConfig.java
@@ -130,6 +130,7 @@ public class NativeConfig {
             "android.view.View#layout",
             "android.view.View#measure",
             "android.view.ViewRootImpl#isInTouchMode",
+            "android.view.ViewRootImpl#performHapticFeedback",
             "android.view.WindowManagerGlobal#getWindowManagerService",
             "android.view.inputmethod.InputMethodManager#isInEditMode",
             "android.view.inputmethod.InputMethodManager#showSoftInput",
