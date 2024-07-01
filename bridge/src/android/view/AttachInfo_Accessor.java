fc34a2783755590e745d5fae8cdae165e6108cfe
Jerome Gaillard <jgaillard@google.com>
2024-07-01 20:36:43 +0100

    Add cutouts to System UI
    This handles cutouts when displaying System UI. The cutout is painted
    black, and is taken into account for the layout/size of the status bar.
    
    Flag: NONE layoutlib for host
    Bug: 335355919
    Test: test added
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:c2b8048c7e25975800dcb4f795f7e04800452100)
    Merged-In: I4b147a6d8f5b8caa46e8ca56c49e237fdd7d799d
    Change-Id: I4b147a6d8f5b8caa46e8ca56c49e237fdd7d799d

--- bridge/src/android/view/AttachInfo_Accessor.java
+++ bridge/src/android/view/AttachInfo_Accessor.java
@@ -45,7 +45,9 @@ public class AttachInfo_Accessor {
         ViewRootImpl_Accessor.setChild(root, view);
         view.assignParent(root);
         if (view instanceof Layout) {
-            InsetUtil.setupSysUiInsets(context, root.getInsetsController(),
+            InsetsController insetsController = root.getInsetsController();
+            wm.createOrUpdateDisplayFrames(insetsController.getState());
+            InsetUtil.setupSysUiInsets(context, insetsController,
                     ((Layout)view).getInsetsFrameProviders());
         }
         view.dispatchAttachedToWindow(info, 0);
