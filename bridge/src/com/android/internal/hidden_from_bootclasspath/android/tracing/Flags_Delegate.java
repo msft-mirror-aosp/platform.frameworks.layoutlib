7e9cb1cc4bec0b16999eb501094a82f7f8904428
Jerome Gaillard <jgaillard@google.com>
2025-04-24 15:05:51 +0100

    Update Flag delegates to handle framework JarJar
    The Android framework build jarjars all the flag classes, adding
    com.android.internal.hidden_from_bootclasspath to their package. This
    updates the Flag_Delegate classes accordingly.
    
    Flag: NONE host-only change
    Bug: 381394200
    Test: layoutlib tests
    Change-Id: Ie22ab41f01a6d0b7349b97a2ec8b7e471c9bcf98

--- bridge/src/com/android/internal/hidden_from_bootclasspath/android/tracing/Flags_Delegate.java
+++ bridge/src/com/android/internal/hidden_from_bootclasspath/android/tracing/Flags_Delegate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 The Android Open Source Project
+ * Copyright (C) 2025 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,13 +14,13 @@
  * limitations under the License.
  */
 
-package android.view.flags;
+package com.android.internal.hidden_from_bootclasspath.android.tracing;
 
 import com.android.tools.layoutlib.annotations.LayoutlibDelegate;
 
 public class Flags_Delegate {
     @LayoutlibDelegate
-    public static boolean sensitiveContentAppProtection() {
+    public static boolean perfettoProtologTracing() {
         return false;
     }
 }
