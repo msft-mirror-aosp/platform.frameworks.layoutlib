aabf08d1942a47055e32d92910625eba12160171
Jerome Gaillard <jgaillard@google.com>
2025-07-01 16:40:30 +0100

    Update automatic monochrome icon generation
    This is to reflect changes in the platform.
    
    Bug: 381394200
    Test: layoutlib tests
    Change-Id: I16b27dc4c0f876c02b07346491a0659f4920b081

--- bridge/src/com/android/launcher3/icons/MonochromeIconFactory_Accessor.java
+++ bridge/src/com/android/launcher3/icons/MonochromeIconFactory_Accessor.java
@@ -30,7 +30,7 @@ public class MonochromeIconFactory_Accessor {
             int foregroundColor) {
         MonochromeIconFactory monoFactory = new MonochromeIconFactory(adaptiveIcon.getBounds().width());
         monoFactory.setColorFilter(new BlendModeColorFilter(foregroundColor, BlendMode.SRC_IN));
-        Drawable mono = monoFactory.wrap(adaptiveIcon, adaptiveIcon.getIconMask());
+        Drawable mono = monoFactory.wrap(adaptiveIcon);
         float inset = getExtraInsetFraction() / (1 + 2 * getExtraInsetFraction());
         return new InsetDrawable(mono, inset);
     }
