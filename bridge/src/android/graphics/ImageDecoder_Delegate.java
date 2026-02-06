de58f3a059bbab8ff7733abdfa5cd63a7e6e2a99
Jerome Gaillard <jgaillard@google.com>
2026-02-06 14:24:46 +0000

    Enable layoutlib-tests to be run with atest
    Instead of using the run_tests.sh script, this allows the tests to be
    run with atest. This means that it will be possible for layoutlib tests
    to be integrated with the standard Android build and test
    infrastructure. This change requires:
    - Add support for JAR files in LayoutlibBridgeClientCallback and TestAssetRepository.
    - Update ImageDecoder_Delegate to use AssetManager for opening non-assets.
    - Generate and use JAR files for test resources and assets in layoutlib-tests.
    - Exclude PerformanceTests from layoutlib-tests host target.
    - Add grpc-java-okhttp dependency to layoutlib-tests.
    
    Bug: N/A
    Test: N/A
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:a6e49b7c8b1c78cd83971d624cf8cbb39965e6f3
    Merged-In: I9cbb42b33b2bdee5b082c451b13df33b55698a3c
    Change-Id: I9cbb42b33b2bdee5b082c451b13df33b55698a3c

--- bridge/src/android/graphics/ImageDecoder_Delegate.java
+++ bridge/src/android/graphics/ImageDecoder_Delegate.java
@@ -22,6 +22,8 @@ import com.android.ninepatch.NinePatch;
 import com.android.tools.layoutlib.annotations.LayoutlibDelegate;
 
 import android.annotation.NonNull;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
 import android.graphics.Bitmap.Config;
 import android.graphics.ImageDecoder.InputStreamSource;
 import android.graphics.ImageDecoder.OnHeaderDecodedListener;
@@ -29,9 +31,10 @@ import android.graphics.ImageDecoder.ResourceSource;
 import android.graphics.ImageDecoder.Source;
 
 import java.awt.image.BufferedImage;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+
+import static android.content.res.AssetManager.ACCESS_STREAMING;
 
 public class ImageDecoder_Delegate {
 
@@ -46,8 +49,14 @@ public class ImageDecoder_Delegate {
         InputStream stream = src instanceof InputStreamSource ?
                 ((InputStreamSource) src).mInputStream : null;
         Bitmap bm = ImageDecoder.decodeBitmapImpl_Original(src, listener);
+        Resources resources = src.getResources();
+        if (resources == null) {
+            return bm;
+        }
         if (stream instanceof NinePatchInputStream && bm.getNinePatchChunk() == null) {
-            stream = new FileInputStream(((NinePatchInputStream) stream).getPath());
+            AssetManager assetManager = resources.getAssets();
+            String path = ((NinePatchInputStream) stream).getPath();
+            stream = assetManager.openNonAsset(0, path, ACCESS_STREAMING);
             NinePatch ninePatch = NinePatch.load(stream, true /*is9Patch*/, false /* convert */);
             BufferedImage image = ninePatch.getImage();
 
