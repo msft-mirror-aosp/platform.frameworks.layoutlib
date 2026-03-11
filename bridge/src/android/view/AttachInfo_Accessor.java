4093f534c86453ed55c72866567fcc0cb42ba5f8
Jerome Gaillard <jgaillard@google.com>
2026-03-11 20:19:03 +0000

    Properly initialize hardware acceleration and traversals
    - Add FLAG_HARDWARE_ACCELERATED to ApplicationInfo and Window
    to ensure ViewRootImpl creates the ThreadedRenderer correctly.
    - Delegate HardwareRenderer.addObserver to avoid native crash.
    - Remove redundant layoutlib workarounds for setting up AttachInfo
    and dispatching pre-draw manually. ViewRootImpl now handles this
    natively through proper traversals.
    
    Flag: EXEMPT HOST_MODE
    Bug: 381394200
    Test: tests updated
    Change-Id: I3822137da5eca14bf6b90c6102e4b90f1f3b76bb

--- bridge/src/android/view/AttachInfo_Accessor.java
+++ bridge/src/android/view/AttachInfo_Accessor.java
@@ -21,70 +21,28 @@ import com.android.layoutlib.bridge.impl.Layout;
 import com.android.layoutlib.bridge.util.InsetUtil;
 
 import android.content.Context;
-import android.graphics.Insets;
-import android.util.Pair;
 import android.view.View.AttachInfo;
-import android.view.Window.OnContentApplyWindowInsetsListener;
 import android.view.WindowManager.LayoutParams;
-
-import static android.view.View.SYSTEM_UI_LAYOUT_FLAGS;
-import static android.view.View.VISIBLE;
 
 /**
  * Class allowing access to package-protected methods/fields.
  */
 public class AttachInfo_Accessor {
-    // Copied from PhoneWindow.java
-    private static final OnContentApplyWindowInsetsListener sDefaultContentInsetsApplier =
-            (view, insets) -> {
-                if ((view.getWindowSystemUiVisibility() & SYSTEM_UI_LAYOUT_FLAGS) != 0) {
-                    return new Pair<>(Insets.NONE, insets);
-                }
-                Insets insetsToApply = insets.getSystemWindowInsets();
-                return new Pair<>(insetsToApply,
-                        insets.inset(insetsToApply).consumeSystemWindowInsets());
-            };
-
     public static LayoutlibRenderer setAttachInfo(ViewGroup view, Layout sysUiLayout) {
         Context context = view.getContext();
         WindowManager wm = (WindowManager)context.getSystemService(Context.WINDOW_SERVICE);
         wm.addView(view, new LayoutParams());
         ViewRootImpl root = (ViewRootImpl) view.getParent();
-        root.setOnContentApplyWindowInsetsListener(sDefaultContentInsetsApplier);
-        LayoutlibRenderer renderer = new LayoutlibRenderer(context, false, "layoutlib-renderer");
-        AttachInfo info = root.mAttachInfo;
-        info.mThreadedRenderer = renderer.getThreadedRenderer();
-        info.mHasWindowFocus = true;
-        info.mWindowVisibility = View.VISIBLE;
-        info.mInTouchMode = false; // this is so that we can display selections.
-        info.mHardwareAccelerated = true;
-        info.mApplicationScale = 1.0f;
-        ViewRootImpl_Accessor.setChild(root, view);
         if (sysUiLayout != null) {
             InsetsController insetsController = root.getInsetsController();
             ((BridgeContext)context).createOrUpdateDisplayFrames(insetsController.getState());
             InsetUtil.setupSysUiInsets(context, insetsController,
                     sysUiLayout.getInsetsFrameProviders());
         }
+
+        AttachInfo info = root.mAttachInfo;
         view.dispatchAttachedToWindow(info, 0);
-        root.mTmpFrames.displayFrame.set(wm.getCurrentWindowMetrics().getBounds());
-        root.mTmpFrames.frame.set(wm.getCurrentWindowMetrics().getBounds());
-        root.mWinFrame.set(wm.getCurrentWindowMetrics().getBounds());
-        return renderer;
-    }
-
-    public static void dispatchOnPreDraw(View view) {
-        view.mAttachInfo.mTreeObserver.dispatchOnPreDraw();
-    }
-
-    public static void dispatchOnPreDraw(View view, ThreadedRenderer renderer) {
-        view.mAttachInfo.mThreadedRenderer = renderer;
-        dispatchOnPreDraw(view);
-    }
-
-    public static void dispatchOnGlobalLayout(View view) {
-        view.dispatchAttachedToWindow(((ViewRootImpl)view.getParent()).mAttachInfo, VISIBLE);
-        view.mAttachInfo.mTreeObserver.dispatchOnGlobalLayout();
+        return new LayoutlibRenderer(info.mThreadedRenderer);
     }
 
     public static void setHasWindowFocus(View view, boolean hasFocus) {
@@ -95,11 +53,5 @@ public class AttachInfo_Accessor {
                 view.dispatchWindowFocusChanged(hasFocus);
             }
         }
-    }
-
-
-
-    public static ViewRootImpl getRootView(View view) {
-        return view.mAttachInfo != null ? view.mAttachInfo.mViewRootImpl : null;
     }
 }
